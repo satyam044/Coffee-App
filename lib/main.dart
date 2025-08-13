892a8dc012486326460ed2ae3421c4926931c242
satyam <mishrasatyam9890@gmail.com>
2025-08-13 19:01:24 +0530

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:coffee_app/screens/splash_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: Color.fromARGB(26, 28, 41, 1),
+      ),
+      home: SplashScreen(),
+    );
+  }
+}
---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Container(
+        padding: EdgeInsets.only(top: 100, bottom: 40),
+        decoration: BoxDecoration(
+          color: Colors.black,
+          image: DecorationImage(
+            image: AssetImage('assets/bg.png'),
+            fit: BoxFit.contain,
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text("Coffee Shop", style: GoogleFonts.pacifico(
+              fontSize: 50,
+              color: Colors.white,
+            ),)
+          ],
+        ),
+      ),
+    );
+  }
+}
