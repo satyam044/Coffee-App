edc016095660f27356d4b11941570726d30ca665
satyam <mishrasatyam9890@gmail.com>
2025-08-13 20:53:36 +0530

    Home Search and TabBar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,9 +7,119 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeScreenState extends State<HomeScreen>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    _tabController = TabController(length: 4, vsync: this, initialIndex: 0);
+    _tabController.addListener(_handleTabSelection);
+    super.initState();
+  }
+
+  _handleTabSelection() {
+    setState(() {});
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.only(top: 15),
+          child: ListView(
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    InkWell(
+                      onTap: () {},
+                      child: Icon(
+                        Icons.sort_rounded,
+                        color: Colors.white,
+                        size: 35,
+                      ),
+                    ),
+                    InkWell(
+                      onTap: () {},
+                      child: Icon(
+                        Icons.notifications,
+                        color: Colors.white,
+                        size: 35,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 30),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 15),
+                child: Text(
+                  "It's a Great Day for Coffee",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+                width: MediaQuery.of(context).size.width,
+                height: 60,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(133, 200, 105, 71),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: "Find Your Coffee",
+                    hintStyle: TextStyle(color: Color.fromARGB(192, 255, 255, 255), fontSize: 20),
+                    prefixIcon: Icon(Icons.search, size: 40, color: Color.fromARGB(192, 255, 255, 255),)
+                  ),
+                ),
+              ),
+              TabBar(
+                controller: _tabController,
+                labelColor: Color.fromARGB(255, 200, 105, 71),
+                unselectedLabelColor: Color.fromARGB(192, 255, 255, 255),
+                indicator: UnderlineTabIndicator(
+                  borderSide: BorderSide(
+                    width: 3,
+                    color: Color.fromARGB(255, 200, 105, 71),
+                  ),
+                  insets: EdgeInsets.symmetric(horizontal: 16),
+                ),
+                tabs: [
+                Tab(text: "Hot Coffee",),
+                Tab(text: "Hot Coffee",),
+                Tab(text: "Hot Coffee",),
+                Tab(text: "Hot Coffee",),
+              ]),
+              SizedBox(height: 10,),
+              Center(
+                child: [
+                  Container(color: Colors.red,),
+                  Container(color: Colors.red,),
+                  Container(color: Colors.red,),
+                  Container(color: Colors.red,),
+                ][_tabController.index],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,6 +1,5 @@
 import 'package:coffee_app/screens/home_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
